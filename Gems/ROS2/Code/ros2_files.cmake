89fce9a9f930aa291d45601f020cb38ae92a469c
Adam Dabrowski <adam.dabrowski@robotec.ai>
2022-03-26 12:48:12 +0100

    Added missing includes
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

--- Gems/ROS2/Code/ros2_files.cmake
+++ Gems/ROS2/Code/ros2_files.cmake
@@ -1,12 +1,12 @@
 
 set(FILES
     Include/ROS2/ROS2Bus.h
-    Source/ROS2ModuleInterface.h
-    Source/ROS2SystemComponent.cpp
-    Source/ROS2SystemComponent.h
-    Source/ROS2LidarSensorComponent.cpp
-    Source/ROS2LidarSensorComponent.h
     Source/LidarRaycaster.cpp
     Source/LidarRaycaster.h
     Source/LidarTemplate.h
+    Source/ROS2LidarSensorComponent.cpp
+    Source/ROS2LidarSensorComponent.h
+    Source/ROS2ModuleInterface.h
+    Source/ROS2SystemComponent.cpp
+    Source/ROS2SystemComponent.h
 )
