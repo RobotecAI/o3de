4e197e20f8aa5329e9cb66f12d0583fa0a4ce16d
Adam Dabrowski <adam.dabrowski@robotec.ai>
2022-03-28 15:44:58 +0200

    Coding standards: copyright
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

--- Gems/ROS2/Code/Source/ROS2EditorSystemComponent.h
+++ Gems/ROS2/Code/Source/ROS2EditorSystemComponent.h
@@ -1,8 +1,12 @@
-
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
 #pragma once
-
 #include <ROS2SystemComponent.h>
-
 #include <AzToolsFramework/Entity/EditorEntityContextBus.h>
 
 namespace ROS2
